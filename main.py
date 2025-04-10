b0c074c316c44ec3edb066bea12f08eca0112642
Brett Cross <9117646+BrettCross@users.noreply.github.com>
2025-04-10 16:35:54 -0400

    add closable display screen

--- main.py
+++ main.py
@@ -2,9 +2,28 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+    # initialize pygame
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill("black")
+
+        #refresh the screen
+        pygame.display.flip()
+
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 # only executes when run directly, won't run if imported as module
 if __name__ == "__main__":
