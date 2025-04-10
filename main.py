bfae694096ad75508da2e3d0454ce202cbd8867c
Brett Cross <9117646+BrettCross@users.noreply.github.com>
2025-04-10 16:38:13 -0400

    remove unnecessary whitespace

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 
-def main():
-    # initialize pygame
-    pygame.init()
 
+def main():
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     # game loop
@@ -17,14 +13,8 @@ def main():
                 return
             
         screen.fill("black")
-
-        #refresh the screen
         pygame.display.flip()
 
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
-# only executes when run directly, won't run if imported as module
 if __name__ == "__main__":
     main()
