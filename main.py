a4de95e7f6c8cff0074d9dee4cd4c36e15ae5271
Brett Cross <9117646+BrettCross@users.noreply.github.com>
2025-04-10 16:15:48 -0400

    add project setup

---
+++ main.py
@@ -0,0 +1,11 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+def main():
+    print("Starting Asteroids!")
+
+# only executes when run directly, won't run if imported as module
+if __name__ == "__main__":
+    main()
